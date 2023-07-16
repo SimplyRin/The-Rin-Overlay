0de5bbc4f7194059d782acb1cbf3e9236300939c
SimplyRin <github@simplyrin.net>
2023-07-17 01:19:14 +0900

    フォーマット

--- src/main/java/net/simplyrin/kzigloader/Main.java
+++ src/main/java/net/simplyrin/kzigloader/Main.java
@@ -478,8 +478,7 @@ public class Main implements ModInitializer {
 			if (bl) {
 				DiffuseLighting.enableGuiDepthLighting();
 			}
-		}
-		catch (Throwable throwable) {
+		} catch (Throwable throwable) {
 			CrashReport crashReport = CrashReport.create(throwable, "Rendering item");
 			CrashReportSection crashReportSection = crashReport.addElement("Item being rendered");
 			crashReportSection.add("Item Type", () -> String.valueOf(stack.getItem()));
